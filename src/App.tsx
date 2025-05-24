6f5f8552057c601eb12738bb083dae3fce9f11c3
rebase <50368758+rebase@users.noreply.github.com>
2025-05-24 18:10:44 +0100

    feat: add monaco editor
    - fix styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Editor from '@monaco-editor/react';
 import {invoke} from '@tauri-apps/api/core';
 import {useState} from 'react';
 import './App.css';
@@ -5,15 +6,19 @@ import './App.css';
 function App() {
   const [formattedString, setFormattedString] = useState('');
   const [isError, setIsError] = useState(false);
+  const [inputString, setInputString] = useState('');
 
-  const formatString = async (string: string) => {
-    if (string.trim() === '') {
+  const formatString = async (value: string | undefined) => {
+    const currentVal = value || '';
+    setInputString(currentVal);
+
+    if (currentVal.trim() === '') {
       setFormattedString('');
       setIsError(false);
       return;
     }
     try {
-      const result = await invoke('format_json_string', {jsonString: string});
+      const result = await invoke('format_json_string', {jsonString: currentVal});
       setFormattedString(result as string);
       setIsError(false);
     } catch (error) {
@@ -24,17 +29,54 @@ function App() {
 
   return (
     <main className="container">
-      <textarea
-        className="input"
-        placeholder="Put your JSON string here"
-        onChange={e => formatString(e.target.value)}
-      />
-      <textarea
-        className={`output ${isError ? 'error' : ''}`}
-        placeholder="Formatted JSON string will appear here"
-        value={formattedString}
-        readOnly
-      />
+      <div className="input editor-wrapper">
+        <Editor
+          height="100%"
+          defaultLanguage="json"
+          theme="vs-light"
+          value={inputString}
+          onChange={formatString}
+          options={{
+            minimap: {enabled: false},
+            fontSize: 14,
+            wordWrap: 'on',
+            automaticLayout: true,
+            scrollBeyondLastLine: false,
+            lineNumbersMinChars: 2,
+            scrollbar: {
+              vertical: 'auto',
+              horizontal: 'auto',
+              arrowSize: 0,
+              useShadows: false,
+            },
+            renderLineHighlight: 'none',
+          }}
+        />
+      </div>
+      <div className={`output editor-wrapper ${isError ? 'error' : ''}`}>
+        <Editor
+          height="100%"
+          defaultLanguage="json"
+          theme="vs-light"
+          value={formattedString}
+          options={{
+            readOnly: true,
+            minimap: {enabled: false},
+            fontSize: 14,
+            wordWrap: 'on',
+            automaticLayout: true,
+            scrollBeyondLastLine: false,
+            lineNumbersMinChars: 3,
+            scrollbar: {
+              vertical: 'auto',
+              horizontal: 'auto',
+              arrowSize: 0,
+              useShadows: false,
+            },
+            renderLineHighlight: 'none',
+          }}
+        />
+      </div>
     </main>
   );
 }
