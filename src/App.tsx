071ba69a59c91dcd5001233ae4ba4d43dfb4aa52
rebase <50368758+rebase@users.noreply.github.com>
2025-05-24 03:46:16 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import {invoke} from '@tauri-apps/api/core';
+import {useState} from 'react';
+import './App.css';
+
+function App() {
+  const [greetMsg, setGreetMsg] = useState('');
+  const [name, setName] = useState('');
+
+  async function greet() {
+    setGreetMsg(await invoke('greet', {name}));
+  }
+
+  return (
+    <main className="container">
+      <form
+        className="row"
+        onSubmit={e => {
+          e.preventDefault();
+          greet();
+        }}>
+        <input
+          id="greet-input"
+          onChange={e => setName(e.currentTarget.value)}
+          placeholder="Enter a name..."
+        />
+        <button type="submit">Greet</button>
+      </form>
+      <p>{greetMsg}</p>
+    </main>
+  );
+}
+
+export default App;
